0f13c29540c1d9693c30d73d9c1ff2bc0c75abe8
kanako-110 <kanako.110.ito@gmail.com>
2021-09-22 17:27:45 +0900

    :art: add
    Task

---
+++ src/components/Header.vue
@@ -0,0 +1,31 @@
+<template>
+	<header>
+		<h1>{{ title }}</h1>
+		<Button text="add Task" color="green" />
+	</header>
+</template>
+
+<script>
+import Button from './Button';
+
+export default {
+	name: 'Header',
+	// props名: type
+	props: {
+		title: String,
+	},
+	components: {
+		Button,
+	},
+};
+</script>
+
+//scope: このコンポーネントの身に適応されるようになる
+<style scoped>
+header {
+	display: flex;
+	justify-content: space-between;
+	align-items: center;
+	margin-block-start: 20px;
+}
+</style>
---
+++ src/components/Task.vue
@@ -0,0 +1,37 @@
+<template>
+	<div :class="[task.reminder ? 'reminder' : '', 'task']">
+		<h3>{{ task.text }}</h3>
+		<p>{{ task.day }}</p>
+		<i class="fas fa-times"></i>
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'Task',
+	props: {
+		// TODO: specific
+		task: Object,
+	},
+};
+</script>
+
+<style scope>
+.fas {
+	color: red;
+}
+.task {
+	background: #f4f4f4;
+	margin: 5px;
+	padding: 10px 20px;
+	cursor: pointer;
+}
+.task.reminder {
+	border-left: 5px solid green;
+}
+.task h3 {
+	display: flex;
+	align-items: center;
+	justify-content: space-between;
+}
+</style>
